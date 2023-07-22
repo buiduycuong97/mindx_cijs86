a30a1b9ca4b2ac55cb888b24da9bfeb22356acc6
buiduycuong97 <duycuongbui97@gmail.com>
2023-07-22 21:26:41 +0700

    f up

---
+++ Lesson1/myapp/src/App.js
@@ -0,0 +1,11 @@
+import Home from "./components/Lesson2/Home.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ Lesson1/myapp/src/components/Lesson2/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  const name = "Mindx";
+
+  return <div>Hello {name}</div>;
+};
+
+export default Home;
